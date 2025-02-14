59d4efc81855e477b6503326c24f26e6e4fb57b0
Ryan Devens <ryan@antarestech.com>
2025-02-14 16:33:19 -0600

    WIP: Added test for AudioFileProcessor.

--- TESTS/test_AudioFileProcessor.cpp
+++ TESTS/test_AudioFileProcessor.cpp
@@ -6,10 +6,16 @@
 #include "../SOURCE/RelativeFilePath.h"
 #include "../SOURCE/BufferFiller.h"
 #include "../SOURCE/BufferHelper.h"
+#include "../SOURCE/AudioFileHelpers.h"
+
 
 static const int kGoldenNumSamples = 1531887;
+static const int kGoldenNumChannels = 1;
+static const int kGoldenBufferNumSamples = 512;
+static const int kFileStartIndex = 88200;
 
-TEST_CASE("Can read/write .wav without processing.")
+//
+TEST_CASE("Can read .wav")
 {
     AudioFileProcessor fileProcessor;
 
@@ -19,27 +25,59 @@ TEST_CASE("Can read/write .wav without processing.")
     // AudioFileProcessor should setup reader/writer/stream and return true
     CHECK(success);
 
-    // Pre-known value for golden file "Somewhere_Mono_441k.wav"
-    int numChannels = 1; // mono per name of golden file
 
     // AudioFileProcessor should update internal mTotalSamples correctly
     CHECK(fileProcessor.getNumReadSamples() == kGoldenNumSamples);
 
-    int numSamples = 512;
     // buffer to read from inputFile and write to outputFile
-    juce::AudioBuffer<float> buffer(numChannels, numSamples);
+    juce::AudioBuffer<float> buffer(kGoldenNumChannels, kGoldenBufferNumSamples);
     buffer.clear();
 
-    fileProcessor.read(buffer, 88200);
+    fileProcessor.read(buffer, kFileStartIndex);
 
     // filling buffer from golden json
-    juce::AudioBuffer<float> goldenBuffer(numChannels, numSamples);
+    juce::AudioBuffer<float> goldenBuffer(kGoldenNumChannels, kGoldenBufferNumSamples);
     auto goldenJsonFile = RelativeFilePath::getGoldenFileFromProjectRoot("GOLDEN_Somewhere_Mono_441k_Two_Second_Mark.json");
     BufferFiller::loadFromJsonFile(goldenJsonFile, goldenBuffer);
 
     // check reading a section of golden wav using read() method agains golden json of chunk of 512 samples in wav file
     bool matchesGolden = BufferHelper::buffersAreIdentical(buffer, goldenBuffer);
     CHECK(matchesGolden);
+
+
+}
+
+
+
+//
+TEST_CASE("Basic read/write")
+{
+    AudioFileProcessor fileProcessor;
+
+    auto inputFile = RelativeFilePath::getGoldenFileFromProjectRoot("GOLDEN_Somewhere_Mono_441k.wav");
+    auto outputFile = RelativeFilePath::getOutputFileFromProjectRoot("AudioFileProcessor_Basic_Read_Write_Test_Output.wav");
+    bool success = fileProcessor.init(inputFile, outputFile);
+
+    // buffer to read from inputFile and write to outputFile
+    juce::AudioBuffer<float> processBuffer(kGoldenNumChannels, kGoldenBufferNumSamples);
+    processBuffer.clear();
+
+    int currentIndex = 0;
+    while(currentIndex < (kGoldenNumSamples - 512))
+    {
+        fileProcessor.read(processBuffer, currentIndex);
+        fileProcessor.write(processBuffer, currentIndex);
+        currentIndex += processBuffer.getNumSamples();
+    }
+
+
+    // use this buffer to read from our new .wav file
+    juce::AudioBuffer<float> testBuffer(kGoldenNumChannels, kGoldenBufferNumSamples);
+    testBuffer.clear();
+
+    AudioFileHelpers::readRange(outputFile, testBuffer, kFileStartIndex);
+
+    CHECK(BufferHelper::buffersAreIdentical(processBuffer, testBuffer));
 
 
 }
