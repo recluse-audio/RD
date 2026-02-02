e141a31fd2fc7cd2492d26977e8e5595a47fa656
Ryan Devens <ryan@antarestech.com>
2026-02-02 13:44:26 -0800

    bus layout changes to work in graph

--- SOURCE/PROCESSORS/GRAIN/GranulatorProcessor.cpp
+++ SOURCE/PROCESSORS/GRAIN/GranulatorProcessor.cpp
@@ -128,24 +128,26 @@ void GranulatorProcessor::releaseResources()
 
 bool GranulatorProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
+    juce::ignoreUnused(layouts); 
     return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
+//   #if JucePlugin_IsMidiEffect
+//     juce::ignoreUnused (layouts);
+//     return true;
+//   #else
+//     // This is the place where you check if the layout is supported.
+//     // In this template code we only support mono or stereo.
+//     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+//      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+//         return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
+//     // This checks if the input layout matches the output layout
+//    #if ! JucePlugin_IsSynth
+//     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+//         return false;
+//    #endif
 
-    return true;
-  #endif
+//     return true;
+//   #endif
 }
 
 void GranulatorProcessor::processBlock (juce::AudioBuffer<float>& buffer,
